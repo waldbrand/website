8deba605f3ece9f9cd19ce28df362bc4c83ce7cd
Sebastian Kürten <sebastian@topobyte.de>
2021-04-27 06:47:28 +0200

    Problem mit fehlendem Icon beheben

--- project/web/src/main/java/de/waldbrand/app/website/icons/Icons.java
+++ project/web/src/main/java/de/waldbrand/app/website/icons/Icons.java
@@ -17,14 +17,8 @@
 
 package de.waldbrand.app.website.icons;
 
-import static de.waldbrand.app.website.icons.Icon.FLACHBRUNNEN;
-import static de.waldbrand.app.website.icons.Icon.HYDRANT;
-import static de.waldbrand.app.website.icons.Icon.SAUGSTELLE;
-import static de.waldbrand.app.website.icons.Icon.SPEICHER;
-import static de.waldbrand.app.website.icons.Icon.TIEFBRUNNEN_E;
-import static de.waldbrand.app.website.icons.Icon.TIEFBRUNNEN_T;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -35,12 +29,7 @@ public class Icons
 
 	private static List<Icon> icons = new ArrayList<>();
 	static {
-		icons.add(FLACHBRUNNEN);
-		icons.add(SAUGSTELLE);
-		icons.add(HYDRANT);
-		icons.add(SPEICHER);
-		icons.add(TIEFBRUNNEN_E);
-		icons.add(TIEFBRUNNEN_T);
+		icons.addAll(Arrays.asList(Icon.values()));
 	}
 
 	private static Map<String, Icon> nameToIcon = new HashMap<>();
