508d1770b0f40cc5f21668e77f95e0db61c6afe5
Sebastian Kürten <sebastian@topobyte.de>
2021-05-27 14:38:01 +0200

    Neuen Server für Deployment verwenden

--- project/deploy.to.production.sh
+++ project/deploy.to.production.sh
@@ -5,4 +5,4 @@ set -e
 ./gradlew clean war
 
 rsync -av web/build/libs/waldbrand-website-web-0.0.1-production.war \
-root@web1.topobyte.de:/opt/tomcat/tomcat-secure/rootapps/waldbrand/ROOT.war
+root@waldbrand-app.de:/opt/tomcat/tomcat-secure/rootapps/waldbrand/ROOT.war
--- project/restart.sh
+++ project/restart.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-ssh root@web1.topobyte.de touch /opt/tomcat/tomcat-secure/rootapps/waldbrand/ROOT.war
+ssh root@waldbrand-app.de touch /opt/tomcat/tomcat-secure/rootapps/waldbrand/ROOT.war
